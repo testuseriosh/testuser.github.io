e16f860f9d5f481af493de474311f0a2c159ee44
testuseriosh <richard.scott@iosh.com>
2023-07-14 14:28:18 +0100

    Preloader

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,15 @@
-const swup = new Swup();
+function init() {
+    // JS here
+  }
+  
+  const swup = new Swup();
+  
+  // Run once when page loads
+  if (document.readyState === 'complete') {
+    init();
+  } else {
+    document.addEventListener('DOMContentLoaded', () => init());
+  }
+  
+  // Run after every additional navigation by swup
+  swup.on('pageView', () => init());
